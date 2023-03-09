1485ee352b3ac623c1f6db851276335d3525ab0d
Dimas Neto <dimas_fcn@outlook.com>
2023-03-08 21:01:21 -0300

    generate pokemons

---
+++ app.js
@@ -0,0 +1,50 @@
+const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+const generatePokemonPromises = () =>
+  Array(150)
+    .fill()
+    .map((_, index) =>
+      fetch(getPokemonUrl(index + 1)).then((response) => response.json())
+    );
+
+const fetchPokemon = () => {
+  const pokemonPromises = generatePokemonPromises();
+
+  // const pokemonPromises = [];
+
+  // for (let i = 1; i <= 150; i++) {
+  //   pokemonPromises.push(
+  //     fetch(getPokemonUrl(i)).then((response) => response.json())
+  //   );
+
+  Promise.all(pokemonPromises).then((pokemons) => {
+    const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+      const types = pokemon.types.map((typeInfo) => typeInfo.type.name);
+
+      accumulator += `
+          <li class="card ${types[0]}">
+            <img class="card-image" alt="${pokemon.name}" 
+              src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${
+                pokemon.id
+              }.png"
+            />
+            <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+            <p class="card-subtitle">${types.join(" | ")}</p>
+          </li>
+        `;
+      return accumulator;
+    }, "");
+
+    const ul = document.querySelector('[data-js="pokedex"]');
+
+    ul.innerHTML = lisPokemons;
+  });
+};
+
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((pokemon) => {
+//     console.log(pokemon);
+//   });
+
+fetchPokemon();
